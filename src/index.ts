9722fc33db889d7491ce22ecf90e7fd3206255ac
Mohammed Sahil Khan <mohammedsahilkhan.msk@gmail.com>
2025-09-26 10:01:28 +0530

    Set up main Express server with route integration

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express"
+import router from "./routes"
+import dotenv from "dotenv"
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 3000
+app.use(router)
+
+app.listen(PORT , ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
+
+
